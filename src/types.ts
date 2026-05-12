42fa896decb3325ddea564d199efbf2d9f0fb318
workwitheduardus <workwitheduardus@gmail.com>
2026-05-12 16:28:49 +0700

    add content types

--- src/types.ts
+++ src/types.ts
@@ -2,7 +2,17 @@
 // Hint: To-Do sebaiknya memiliki id, text, dan status completed
 
 // TODO: Buat interface untuk To-Do item
-
+export interface Todo {
+  id: number;
+  text: string;
+  status: TodoStatus;
+  createdAt: string;
+}
 // TODO: Buat tipe untuk status To-Do (active/done)
+export type TodoStatus = "active" | "done";
 
 // TODO: Buat tipe untuk fungsi-fungsi yang akan digunakan
+export type AddTodoFunction = (text: string) => Todo;
+export type CompleteToFoFunction = (id: number) => Todo | null;
+export type DeleteTodoFunction = (id: number) => boolean;
+export type ListTodosFunction = () => Todo[];
